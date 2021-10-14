13298b478847ff2a8d4a51de44a44d5c5ee1c3f7
Louis Spencer <u1862808@unimail.hud.ac.uk>
2021-10-14 21:43:26 +0100

    Add a visitor model with migrations and a seeder. Add to controller and view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'title' => 'Hello World!!',
+        'content' => 'Lorem Ipsum...'
+    ]);
 });
+*/
+
+Route::get('/', [WelcomeController::class, 'index']);
